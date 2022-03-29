a3e5ec2469b71379196fec641bd474d2e55da477
SwiftBinaryCode <89089012+SwiftBinaryCode@users.noreply.github.com>
2022-03-29 09:19:17 +0200

    Pushed base project to main branch

---
+++ KärraGamesCorner/KärraGamesCorner/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace KärraGamesCorner.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
