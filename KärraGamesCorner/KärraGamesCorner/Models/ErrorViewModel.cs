86cd90d591057beacfb02cf07899ada03d6802cb
SwiftBinaryCode <89089012+SwiftBinaryCode@users.noreply.github.com>
2022-04-04 14:42:45 +0200

    deleted project

--- KärraGamesCorner/KärraGamesCorner/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace KärraGamesCorner.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
