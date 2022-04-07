35534cfdd703c99bc46e856961e35b96b7c084fc
Martincoder24 <80486477+Martincoder24@users.noreply.github.com>
2022-04-07 15:51:54 +0200

    ab#127 Lagt till genre med has Data

--- KärraGamesCorner/KärraGamesCorner/Data/ApplicationDbContext.cs
+++ KärraGamesCorner/KärraGamesCorner/Data/ApplicationDbContext.cs
@@ -24,14 +24,31 @@ namespace KärraGamesCorner.Data
         {
             //string fileName = "Resources/Produkter.Json";
             //string readAllText = File.ReadAllText(fileName);
+            modelBuilder
+                .Entity<Genre>().HasData(
+                    new Genre (){Id = 1, Name = "Rollspel"},
+                    new Genre() { Id = 2, Name = "Pussel" },
+                    new Genre() { Id = 3, Name = "Sällskapsspel" },
+                    new Genre() { Id = 4, Name = "Actionrollspel" },
+                    new Genre() { Id = 5, Name = "Kortspel" },
+                    new Genre() { Id = 6, Name = "Skräck" },
+                    new Genre() { Id = 7, Name = "Äventyr" },
+                    new Genre() { Id = 8, Name = "Strategi" },
+                    new Genre() { Id = 9, Name = "FPS" },
+                    new Genre() { Id = 10, Name = "Överlevnad" },
+                    new Genre() { Id = 11, Name = "MMORPG" },
+                    new Genre() { Id = 12, Name = "Racing" },
+                    new Genre() { Id = 13, Name = "Simulator" });
 
+            //Använd inte konstruktorn när du lägger till produkter
             //Product[] productList = Newtonsoft.Json.JsonConvert.DeserializeObject<Product[]>(readAllText);
-            //var prod = new Product[] { new Product(1, "bla", "bla", 20, new Genre(), "", ""), };
-            //new Product(2, "bla", "bla", 20, new Genre(), "", "");
+            var prod = new Product[] { new Product(1, "bla", "bla", 20, new Genre(), "", ""), };
+            new Product(2, "bla", "bla", 20, GenreId = 2, "", "");
 
             modelBuilder
-                .ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly())
                 .Entity<Product>().HasData(prod);
+
+            base.OnModelCreating(modelBuilder);
                     
             
         }
