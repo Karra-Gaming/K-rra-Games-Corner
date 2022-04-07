3a0cb3111b65078d2f2b257ff4e0e481f2fac1f2
ErikVerta <89127095+ErikVerta@users.noreply.github.com>
2022-04-07 14:48:40 +0200

    ab#113 Added admin account

--- KärraGamesCorner/KärraGamesCorner/Data/ApplicationDbContext.cs
+++ KärraGamesCorner/KärraGamesCorner/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using KärraGamesCorner.Data.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -16,12 +17,44 @@ namespace KärraGamesCorner.Data
         {
         }
 
-        //protected override void OnModelCreating(ModelBuilder builder)
-        //{
-        //    builder.Entity<ApplicationUser>(u =>
-        //    {
-        //        u.HasMany<Product>().WithMany <ApplicationUser>()
-        //    });
-        //}
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            Guid ADMIN_ID = Guid.NewGuid();
+            string ROLE_ID = Guid.NewGuid().ToString();
+
+            //seed admin role
+            builder.Entity<IdentityRole>().HasData(new IdentityRole
+            {
+                Name = "Admin",
+                NormalizedName = "ADMIN",
+                Id = ROLE_ID,
+                ConcurrencyStamp = ROLE_ID
+            });
+
+            //create user
+            var appUser = new ApplicationUser()
+            {
+                Id = ADMIN_ID,
+                Email = "admin@gmail.com",
+                EmailConfirmed = true,
+                UserName = "admin@gmail.com",
+                NormalizedUserName = "ADMIN@GMAIL.COM"
+            };
+
+            //set user password
+            PasswordHasher<ApplicationUser> ph = new PasswordHasher<ApplicationUser>();
+            appUser.PasswordHash = ph.HashPassword(appUser, "admin");
+
+            //seed user
+            builder.Entity<ApplicationUser>().HasData(appUser);
+
+            //set user role to admin
+            builder.Entity<IdentityUserRole<string>>().HasData(new IdentityUserRole<string>
+            {
+                RoleId = ROLE_ID,
+                UserId = ADMIN_ID.ToString()
+            });
+            base.OnModelCreating(builder);
+        }
     }
 }
