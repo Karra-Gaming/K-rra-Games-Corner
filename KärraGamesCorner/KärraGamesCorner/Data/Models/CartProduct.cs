db169265ee17ba4de6dbe37a0cc0e1cda069f771
SwiftBinaryCode <89089012+SwiftBinaryCode@users.noreply.github.com>
2022-04-12 11:22:37 +0200

    changed prod class and added cartproduct class.

---
+++ KärraGamesCorner/KärraGamesCorner/Data/Models/CartProduct.cs
@@ -0,0 +1,16 @@
+﻿namespace KärraGamesCorner.Data.Models
+{
+    public class CartProduct
+    {
+        public int UserId { get; set; }
+        public int ProductId { get; set; }
+        public int Amount { get; set; }
+
+        public CartProduct()
+        {
+
+        }
+
+
+    }
+}
